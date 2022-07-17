20988bb0a8bbc72001356ca37b01b3e44b7d0f64
Valentine <valentine.nazarov@gmail.com>
2022-07-17 13:56:21 +0300

    feat: add basic server configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('v1/api');
+  await app.listen(PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+
+bootstrap().catch((error) => console.log(error));
