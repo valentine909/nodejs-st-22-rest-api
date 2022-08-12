a75ea53ac8e82d5a5c11e5ff1dd6f517e1d8d27b
Valentine <valentine.nazarov@gmail.com>
2022-08-12 17:14:31 +0300

    feat: add logging wrapper and refactor service logger

--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -3,66 +3,35 @@ import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
 import { GroupsDataManager } from './data-manager/groups.data.manager';
 import { Group } from './entities/group.entity';
-import { ServiceLogger } from '../utils/service.logger';
+import { loggingWrapper } from '../utils/logging.wrapper';
 
 @Injectable()
 export class GroupsService {
-  constructor(
-    private groupDataManager: GroupsDataManager,
-    private logger: ServiceLogger,
-  ) {}
+  constructor(private groupDataManager: GroupsDataManager) {}
 
   async create(createGroupDto: CreateGroupDto): Promise<Group> {
-    this.logger.log(
-      this.constructor.name,
-      this.create.name,
-      Array.from(arguments),
-    );
     return this.groupDataManager.create(createGroupDto);
   }
 
   async findAll(): Promise<Group[]> {
-    this.logger.log(
-      this.constructor.name,
-      this.findAll.name,
-      Array.from(arguments),
-    );
     return this.groupDataManager.findAll();
   }
 
   async findOne(id: string): Promise<Group> {
-    this.logger.log(
-      this.constructor.name,
-      this.findOne.name,
-      Array.from(arguments),
-    );
     return this.groupDataManager.findById(id);
   }
 
   async update(id: string, updateGroupDto: UpdateGroupDto): Promise<Group> {
-    this.logger.log(
-      this.constructor.name,
-      this.update.name,
-      Array.from(arguments),
-    );
     return this.groupDataManager.update(id, updateGroupDto);
   }
 
   async delete(id: string): Promise<number> {
-    this.logger.log(
-      this.constructor.name,
-      this.delete.name,
-      Array.from(arguments),
-    );
     return this.groupDataManager.delete(id);
   }
 
   async addUsers(id: string, updateGroupDto: UpdateGroupDto) {
-    this.logger.log(
-      this.constructor.name,
-      this.addUsers.name,
-      Array.from(arguments),
-    );
     return this.groupDataManager.addUsers(id, updateGroupDto);
   }
 }
+
+loggingWrapper(GroupsService.prototype);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,21 +3,13 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { UsersDataManager } from './data-manager/users.data.manager';
-import { ServiceLogger } from '../utils/service.logger';
+import { loggingWrapper } from '../utils/logging.wrapper';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    private usersDataManager: UsersDataManager,
-    private logger: ServiceLogger,
-  ) {}
+  constructor(private usersDataManager: UsersDataManager) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    this.logger.log(
-      this.constructor.name,
-      this.create.name,
-      Array.from(arguments),
-    );
     return this.usersDataManager.create(createUserDto);
   }
 
@@ -25,38 +17,20 @@ export class UsersService {
     loginSubstring: string,
     limit: number,
   ): Promise<User[]> {
-    this.logger.log(
-      this.constructor.name,
-      this.getAutoSuggestUsers.name,
-      Array.from(arguments),
-    );
     return this.usersDataManager.findSuggested(limit, loginSubstring);
   }
 
   async findOne(id: string): Promise<User> {
-    this.logger.log(
-      this.constructor.name,
-      this.findOne.name,
-      Array.from(arguments),
-    );
     return this.usersDataManager.findById(id);
   }
 
   async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
-    this.logger.log(
-      this.constructor.name,
-      this.update.name,
-      Array.from(arguments),
-    );
     return this.usersDataManager.update(id, updateUserDto);
   }
 
   async delete(id: string): Promise<number> {
-    this.logger.log(
-      this.constructor.name,
-      this.delete.name,
-      Array.from(arguments),
-    );
     return this.usersDataManager.delete(id);
   }
 }
+
+loggingWrapper(UsersService.prototype);
