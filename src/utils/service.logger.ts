8cecec2bb027fd549b054a86afe71d295e9ec37f
Valentine <valentine.nazarov@gmail.com>
2022-08-12 15:50:43 +0300

    refactor: refactor ServiceLogger

--- src/utils/service.logger.ts
+++ src/utils/service.logger.ts
@@ -1,11 +1,11 @@
 import { markYellow } from './for.console.log';
 
 export class ServiceLogger {
-  log(service: string, method: string, args: any[]) {
-    console.log(
-      `${markYellow('Service:')} ${service}, ${markYellow(
-        'method:',
-      )} ${method}, ${markYellow('arguments:')} ${JSON.stringify(args)}`,
-    );
+  log(service: string, method: string, args: any[], error = '') {
+    const serviceMessage = `${markYellow('Service:')} ${service}`;
+    const methodMessage = `${markYellow('method:')} ${method}`;
+    const argsMessage = `${markYellow('arguments:')} ${JSON.stringify(args)}`;
+    const errorMessage = error ? `${markYellow('error:')} ${error}` : '';
+    console.log(serviceMessage, methodMessage, argsMessage, errorMessage);
   }
 }
