25782159b7866c7e0c3318fcab887fc1ab754e12
Valentine <valentine.nazarov@gmail.com>
2022-08-14 22:19:49 +0300

    feat: add http error logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './data-source';
 import { GroupsModule } from './groups/groups.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { LogInterceptor } from './log.interceptor';
+import { AllExceptionsFilter } from './utils/exceptions.filter';
 
 @Module({
   imports: [
@@ -16,6 +17,10 @@ import { LogInterceptor } from './log.interceptor';
     {
       provide: APP_INTERCEPTOR,
       useClass: LogInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
     },
   ],
 })
